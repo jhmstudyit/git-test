b16bf2b6e8c9351b60f8ebb21b8d8a1ac6b55548
Jiahm <1515254361@qq.com>
2022-12-09 10:38:10 +0800

    second commit

--- src/main/java/com/itheima/git/GitTest.java
+++ src/main/java/com/itheima/git/GitTest.java
@@ -3,5 +3,6 @@ package com.itheima.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Jiahm study Git!");
     }
 }
